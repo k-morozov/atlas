c0c974d74baf8e3dcf698be88e6c1d5c2033d6ab
Konstantin Morozov <34001730+k-morozov@users.noreply.github.com>
2025-02-02 09:38:50 +0100

    example (#17)

---
+++ examples/example.rs
@@ -0,0 +1,51 @@
+use rand::Rng;
+
+use kvs::core::entry::Entry;
+use kvs::core::field::{Field, FieldType};
+use kvs::core::table::simple_table::SimpleTable;
+use kvs::core::table::{config::TableConfig, table::Table};
+
+const TOTAL_VALUE: usize = 1000000;
+const K: i32 = 117;
+
+fn main() {
+    let table_name = "example_table";
+    let config = TableConfig::new_config(512, 16);
+    let mut table = SimpleTable::new(table_name, config);
+
+    for index in 0..TOTAL_VALUE {
+        let entry = Entry::new(
+            Field::new(FieldType::Int32(index as i32)),
+            Field::new(FieldType::Int32((index as i32) * K)),
+        );
+        table.put(entry).unwrap();
+    }
+
+    println!("Data was inserted.");
+
+    for index in (0..TOTAL_VALUE).step_by(10000) {
+        println!("Start searching index={}", index);
+        let result = table
+            .get(Field::new(FieldType::Int32(index as i32)))
+            .unwrap();
+
+        assert_eq!(
+            result.unwrap(),
+            Field::new(FieldType::Int32((index as i32) * K))
+        );
+    }
+
+    // for index in 0..100 {
+    //     println!("Start searching index={}", index);
+
+    //     for j in (index..TOTAL_VALUE).step_by(100) {
+    //         println!("Searching index={}, j={}", index, j);
+    //         let result = table.get(Field::new(FieldType::Int32(j as i32))).unwrap();
+
+    //         assert_eq!(
+    //             result.unwrap(),
+    //             Field::new(FieldType::Int32((j as i32) * K))
+    //         );
+    //     }
+    // }
+}
--- src/core/table/config.rs
+++ src/core/table/config.rs
@@ -1,3 +1,25 @@
 pub const DEFAULT_TABLES_PATH: &'static str = "/tmp/kvs/tables/";
 pub const DETAULT_MEM_TABLE_SIZE: usize = 4;
 pub const DEFAULT_SEGMENTS_LIMIT: usize = 4;
+
+#[derive(Clone)]
+pub struct TableConfig {
+    pub mem_table_size: usize,
+    pub segments_limit: usize,
+}
+
+impl TableConfig {
+    pub fn new_config(mem_table_size: usize, segments_limit: usize) -> Self {
+        TableConfig {
+            mem_table_size,
+            segments_limit,
+        }
+    }
+
+    pub fn default_config() -> Self {
+        TableConfig {
+            mem_table_size: DETAULT_MEM_TABLE_SIZE,
+            segments_limit: DEFAULT_SEGMENTS_LIMIT,
+        }
+    }
+}
