161f73877b737cc353dca5439693eccbdffe7ae0
Konstantin Morozov <just.morozov.k@gmail.com>
2025-02-27 19:35:44 +0100

    up

--- src/core/storage/ordered_storage.rs
+++ src/core/storage/ordered_storage.rs
@@ -12,7 +12,7 @@ use crate::core::field::FlexibleField;
 use crate::core::mem_table::MemoryTable;
 use crate::core::merge::merge::{is_ready_to_merge, merge_disk_tables};
 use crate::core::storage::{
-    config::{StorageConfig, DEFAULT_TEST_TABLES_PATH},
+    config::StorageConfig,
     metadata::StorageMetadata,
 };
 use crate::errors::Error;
@@ -102,7 +102,7 @@ impl OrderedStorage {
 
 impl Storage for OrderedStorage {
     fn put(&mut self, entry: FlexibleUserEntry) -> Result<(), Error> {
-        self.mem_table.append(entry.clone());
+        self.mem_table.append(entry);
 
         if self.mem_table.current_size() == self.mem_table.max_table_size() {
             self.save_mem_table();
