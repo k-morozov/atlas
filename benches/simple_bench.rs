129c974d9ca4a97c900b49796be52827b003cb82
Konstantin Morozov <just.morozov.k@gmail.com>
2025-03-09 21:04:14 +0100

    up

--- benches/simple_bench.rs
+++ benches/simple_bench.rs
@@ -111,8 +111,7 @@ fn example_table() {
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("sample-size-example");
-    group.sample_size(10);
+    let mut group = c.benchmark_group("simple-run");
     group.measurement_time(Duration::new(1800, 0));
     group.bench_function("my benchmark", |b| b.iter(|| example_table() ));
     group.finish();
